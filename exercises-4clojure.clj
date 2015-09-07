1d8cd694a5576f6e6bc32086a26221a53efb2705
Thomas Harren <harre096@umn.edu>
2015-09-07 09:16:32 -0500

    formatted file, solved first several

--- exercises-4clojure.clj
+++ exercises-4clojure.clj
@@ -1,3 +1,4 @@
+;;;Thomas Harren
 ;;; Put your solutions to the assigned 4Clojure exercises here and commit
 ;;; back up to your fork on Github.
 
@@ -6,30 +7,149 @@
 ;;; scratch computations you found helpful, etc.), SO MAKE SURE YOU LABEL YOUR
 ;;; "ANSWERS" clearly.
 
-;;; I've included a few examples below just to give you a sense of what I'm looking
-;;; for.
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;; Problem #162: Logical falsity and true
+;;; Problem 10 - Intro to Maps
 
 ; What I pasted in the box:
 
-1
+20
 
-; because all the given expressions returned 1, for example:
+; Two ways of using the same hashmap. 20 is the value on the key ":b":
 
-(= 1 (if [] 1 0))
+(= __ ((hash-map :a 10, :b 20, :c 30) :b))
+(= __ (:b {:a 10, :b 20, :c 30}))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;; Problem #15: Double Down
+;;;Problem 16 - Hello World
 
 ; What I pasted in the box:
 
-#(* 2 %)
+#(str "Hello, " % "!")
 
-; which is an anonymous function that doubles its argument, for example:
+;I used short notation to write an function that parses the strings togather. 
 
-(= (#(* 2 %) 11) 22)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;Problem 17 - Sequences: map
+
+; What I pasted in the box:
+
+'(6 7 8)
+
+; We are mapping a function to a list of numbers that adds five to each number 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;Problem 18 - Sequences: filter
+
+; What I pasted in the box:
+
+'(6 7)
+
+; we filtered through the list keeping only numbers greater that five: #(> % 5), thus we only have the bigger numbers left in the new list
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;Problem 20 - Penultimate Element
+
+; What I pasted in the box:
+
+
+
+; 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;Problem 21 - Nth Element
+
+; What I pasted in the box:
+
+
+
+; 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;Problem 23 - Reverse a Sequence
+
+; What I pasted in the box:
+
+
+
+; 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;Problem 25 - Find the odd numbers
+
+; What I pasted in the box:
+
+
+
+; 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;Problem 37 - Regular Expressions
+
+; What I pasted in the box:
+
+
+
+; 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;Problem 40 - Interpose a Seq
+
+; What I pasted in the box:
+
+
+
+; 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;Problem 43 - Reverse Interleave
+
+; What I pasted in the box:
+
+
+
+; 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;Problem 44 - Rotate Sequence
+
+; What I pasted in the box:
+
+
+
+; 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;Problem 46 - Flipping out
+
+; What I pasted in the box:
+
+
+
+; 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;Problem 47 - Contain yourself
+
+; What I pasted in the box:
+
+
+
+; 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
